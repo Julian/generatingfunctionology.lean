ccc841b335a9e55f4f60020a757a00afc1fd148f
Julian Berman <Julian@GrayVines.com>
2024-08-17 14:34:50 -0400

    Fill out the first statement.

--- Generatingfunctionology/Chapter1.lean
+++ Generatingfunctionology/Chapter1.lean
@@ -1,4 +1,6 @@
-import Mathlib.RingTheory.PowerSeries.Basic
+import Mathlib.RingTheory.PowerSeries.Inverse
+
+noncomputable section
 
 open PowerSeries
 
@@ -9,9 +11,10 @@ abbrev α : ℕ → ℚ
 | n + 1 => 2 * (α n) + 1
 | 0 => 0
 
-def A : ℚ⟦X⟧ := mk (α ·)
+-- A(x)
+def A : ℚ⟦X⟧ := mk α
 
-example : A = A := by
-  rfl
+lemma A_eq : A = X * (1 - X)⁻¹ * (1 - 2 * X)⁻¹ := sorry
+lemma α_eq (n : ℕ) : α n = 2 ^ n - 1 := sorry
 
 #check A
