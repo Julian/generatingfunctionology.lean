685dca17476cb26123d1ed1409d17818474ac27b
Julian Berman <Julian@GrayVines.com>
2024-08-17 14:37:48 -0400

    Partial fraction as well.

--- Generatingfunctionology/Chapter1.lean
+++ Generatingfunctionology/Chapter1.lean
@@ -15,6 +15,12 @@ abbrev α : ℕ → ℚ
 def A : ℚ⟦X⟧ := mk α
 
 lemma A_eq : A = X * (1 - X)⁻¹ * (1 - 2 * X)⁻¹ := sorry
+
+abbrev F₁ : ℚ⟦X⟧ := 2 * (1 - X)⁻¹
+abbrev F₂ : ℚ⟦X⟧ := 1 * (1 - X)⁻¹
+
+lemma A_frac : X * (1 - X)⁻¹ * (1 - 2 * X)⁻¹ = X * (F₁ - F₂) := sorry
+
 lemma α_eq (n : ℕ) : α n = 2 ^ n - 1 := sorry
 
 #check A
