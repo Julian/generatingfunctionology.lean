768cdbfb68dd95d240f1f66c03e02ea794bcd4a9
Noah Stebbins <nsteb1993@gmail.com>
2024-08-22 21:26:56 -0400

    A_frac, remove X

--- Generatingfunctionology/Chapter1.lean
+++ Generatingfunctionology/Chapter1.lean
@@ -16,10 +16,24 @@ def A : ℚ⟦X⟧ := mk α
 
 lemma A_eq : A = X * (1 - X)⁻¹ * (1 - 2 * X)⁻¹ := sorry
 
-abbrev F₁ : ℚ⟦X⟧ := 2 * (1 - X)⁻¹
+abbrev F₁ : ℚ⟦X⟧ := 2 * (1 - 2 * X)⁻¹
 abbrev F₂ : ℚ⟦X⟧ := 1 * (1 - X)⁻¹
 
-lemma A_frac : X * (1 - X)⁻¹ * (1 - 2 * X)⁻¹ = X * (F₁ - F₂) := sorry
+#check X
+#check (X : ℚ⟦X⟧)
+#check F₁
+#check A
+
+lemma cancel_left (Y Z : ℚ⟦X⟧) : Y = Z → X * Y = X * Z := by
+  intro h
+  exact congrArg (HMul.hMul X) h
+
+lemma A_frac : X * (1 - X)⁻¹ * (1 - 2 * X)⁻¹ = X * (F₁ - F₂) := by
+  rw [F₁, F₂, mul_assoc]
+  apply cancel_left
+  sorry
+  -- todo: partial fraction expansion
+
 
 lemma α_eq (n : ℕ) : α n = 2 ^ n - 1 := sorry
 
