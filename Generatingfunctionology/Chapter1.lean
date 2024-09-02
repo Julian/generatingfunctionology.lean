7cb816176104b8a6d71a8ace97550a96a67e85b7
Eric P <eric.1334@icloud.com>
2024-09-02 12:51:59 -0400

    First example

--- Generatingfunctionology/Chapter1.lean
+++ Generatingfunctionology/Chapter1.lean
@@ -9,26 +9,40 @@ namespace PowerSeries
 
 variable {α : Type*} [Semiring α] (φ φ' : PowerSeries α)
 
-def shift : PowerSeries α := PowerSeries.mk fun n ↦ coeff α (n + 1) φ
+/- Same as normal but ring is implicit -/
+abbrev coeff' := coeff α
+abbrev C' := C α
+abbrev constantCoeff' := constantCoeff α
 
-lemma shift_def : φ.shift = PowerSeries.mk fun n ↦ coeff α (n + 1) φ := rfl
+/-
+  Given `p = a_0 + a_1 * X + a_2 * X^2 + ...`,
+  `p.shift = a_1 + a_2 * X + a_3 * X^2 + ...`
+-/
+abbrev shift : PowerSeries α := PowerSeries.mk fun n ↦ coeff' (n + 1) φ
 
 namespace shift
 
-@[simp]
-lemma shift_mul_X_add : φ.shift * X + (C α (constantCoeff α φ)) = φ := by
+/-
+  If `p = a_0 + a_1 * X + a_2 * X^2 + ...`, then
+  `p.shift * X + a_0 = p`
+-/
+lemma shift_mul_X_add : φ.shift * X + (C' (constantCoeff' φ)) = φ := by
   ext n; cases n <;> simp
 
 variable {φ φ'}
 
-lemma foo (h: φ.shift = φ') : φ = φ' * X + (C α (constantCoeff α φ)) := by
-  have : φ.shift * X + (C α (constantCoeff α φ)) = φ' * X + (C α (constantCoeff α φ)) := by exact congrFun (congrArg HAdd.hAdd (congrFun (congrArg HMul.hMul h) X)) ((C α) ((constantCoeff α) φ))
+/-
+  Given `φ = a_0 + a_1 * X + a_2 * X^2 + ...`,
+  if `φ.shift = φ'`, then
+  `φ = φ' * X + a_0`
+-/
+lemma shift_inv (h: φ.shift = φ') : φ = φ' * X + (C' (constantCoeff' φ)) := by
+  have : φ.shift * X + (C' (constantCoeff' φ)) = φ' * X + (C' (constantCoeff' φ)) := by
+    exact congrFun (congrArg HAdd.hAdd (congrFun (congrArg HMul.hMul h) X)) (C' (constantCoeff' φ))
   rw [shift_mul_X_add] at this
   assumption
 
 end shift
-
-#check X⁻¹
 
 end PowerSeries
 
@@ -39,80 +53,90 @@ abbrev α : ℕ → ℚ
 -- A(x)
 abbrev A : ℚ⟦X⟧ := mk α
 
-abbrev left : ℚ⟦X⟧ := mk fun n ↦ α (n + 1)
-abbrev right : ℚ⟦X⟧ := mk fun n ↦ 2 * α n + 1
+section invOneScaled
 
--- In the book, this is A / X -- but Mathlib does not define division by X; and it defines
--- X⁻¹ to be 0 (see `X_inv`).
--- So for now we get around thinking too hard by instead talking about X * LHS.
-lemma left_eq : A.shift = mk (fun n ↦ α (n + 1)) := by simp [shift_def]
+universe u
+variable {R : Type u} [Ring R]
 
-lemma right_eq : mk (2 * α · + 1) = 2 * A + invUnitsSub 1 := calc
-  _ = mk (2 * α ·) + invUnitsSub 1 := by ext; simp
-  _ = 2 * A + invUnitsSub 1 := by
-    ext
-    have : ↑2 = C ℚ 2 := rfl
-    simp [this]
+/-
+  The power series for `(1 - a*X)⁻¹`.
+  (It's equal to `1 + a * X + a^2 * X^2 + ...`)
+-/
+def invOneScaled (a : R) : PowerSeries R :=
+  mk fun n => a^n
 
-example : A.shift = 2 * A + invUnitsSub 1 → A = X * (1 - X)⁻¹ * (1 - 2 * X)⁻¹ := by
-  intro h
-  have := shift.foo h
-  simp at this
-  rw [this, A]
-  have one_eq : mk 1 = invUnitsSub (1 : ℚˣ) := by ext; simp
-  rw [← one_eq] at this
-  have swap : X * (2 * A + mk 1) = (2 * A + mk 1) * X := by ring
-  rw [←swap] at this
-  have plzring : X * (2 * A + mk 1) = X * 2 * A + X * mk 1 := by ring
-  have plzring' : A - X * 2 * A = X * mk 1 := by exact sub_eq_of_eq_add' this
-  have plzring'' : A - X * 2 * A = A * (1 - 2 * X) := by ring
-  rw [plzring, ←plzring', plzring''] at this
-
-lemma A_eq : A = X * (1 - X)⁻¹ * (1 - 2 * X)⁻¹ := sorry
-
-abbrev A_frac : ℚ⟦X⟧ := X * ((2 * (1 - 2 * X)⁻¹) - (1 - X)⁻¹)
-
-lemma A_eq_frac : A = A_frac := by
-  symm
-  rw [A_eq, A_frac]
-  calc (X * (2 * (1 - 2 * X)⁻¹ - (1 - X)⁻¹) : ℚ⟦X⟧)
-    _ = X * 2 * (1 - 2 * X)⁻¹ - X * (1 - X)⁻¹ := by ring
-    _ = X * 2 * (1 - 2 * X)⁻¹ - X * (1 - X)⁻¹ := rfl
-    _ = X * (1 - X)⁻¹ * (1 - 2 * X)⁻¹ := sorry
-
-lemma α_eq (n : ℕ) : α n = 2 ^ n - 1 := by
-  -- "Multiply the left side by x^n and sum", which in Mathlib is "form the power series with coeff⋯"
-  have A_eq : mk (fun n ↦ α (n + 1)) = 2 * A + invUnitsSub 1 := right_eq
-
-  have : X * mk (fun n ↦ α (n + 1)) = X * 2 * A + invUnitsSub 1 := by
-    sorry
-  rw [left_eq] at this
-
-  have h' : mk (fun n ↦ 2 * (α n) + 1) = mk (fun n ↦ 2 * (α n)) + mk 1 := rfl
-  have h'' : mk (fun n ↦ 2 * (α n)) = 2 * A := by
-    have : ↑2 = C ℚ 2 := rfl
-    ext n
-    simp [coeff_mk, this, coeff_C_mul]
-  have : mk 1 = invUnitsSub (1 : ℚˣ) := by ext; simp
-  have h''' : mk (fun n ↦ 2 * (α n)) + mk 1 = 2 * A + invUnitsSub 1 := by rw [h'', this]
-
-  sorry
-
-#check A
-
-#exit
-
-example : mk (fun n ↦ α (n + 1)) = X⁻¹ * A := by
-  rw [eq_shift_mul_X_add_const A]
-  simp only [coeff_mk, constantCoeff_mk, map_zero, add_zero, zero_mul]
-  rw [mul_comm _ X, ←mul_assoc]
-  simp [left]
-
-example : right = 2 * A + (1 - X)⁻¹ := by
-  dsimp [right]
-  ext n
-  have : ↑2 = C ℚ 2 := rfl
-  simp_all
-
+/-
+  The constant coefficient of `(1 - a*X)⁻¹` is 1
+-/
 @[simp]
-lemma foo (n : ℚ) : (n : ℚ⟦X⟧) = C ℚ n := rfl
+theorem constCoeff_invOneScaled (a : R) : constantCoeff' (invOneScaled a) = 1 := by
+  simp [constantCoeff', invOneScaled]
+
+/-
+  `(1 - a*X)⁻¹ * a*X = a*X + a^2*X^2 + a^3*X^3 + ...`
+-/
+theorem mul_invOneScaled_scale_shifts (a : R) : (invOneScaled a) * (C' a * X) = mk fun n => if n = 0 then 0 else a^n := by
+  simp [invOneScaled, ←mul_assoc]
+  ext n
+  cases' n with n
+  <;> simp [pow_succ]
+
+/-
+  `a*X * (1 - a*X)⁻¹ = a*X + a^2*X^2 + a^3*X^3 + ...`
+  (Need both sides since not assuming that `R` is a commutative ring)
+-/
+theorem mul_invOneScaled_scale_shifts' (a : R) : (C' a * X) * (invOneScaled a) = mk fun n => if n = 0 then 0 else a^n := by
+  simp [invOneScaled, mul_assoc]
+  ext n
+  cases' n with n
+  <;> simp [pow_succ, pow_mul_comm']
+
+/-
+  `(1 - a*X) * (1 - a*X)⁻¹ = 1`
+-/
+theorem invOneScaled_inv (a : R) : (1 - C' a * X) * invOneScaled a = 1 := by
+  ext n
+  cases' n with n
+  · simp
+  · simp [sub_mul 1 (C' a * X) (invOneScaled a), mul_invOneScaled_scale_shifts']
+    simp [invOneScaled]
+
+/-
+  `(1 - a*X)⁻¹ * (1 - a*X) = 1`
+-/
+theorem invOneScaled_inv' (a : R) : (invOneScaled a) * (1 - C' a * X) = 1 := by
+  ext n
+  cases' n with n
+  · simp
+  · simp [mul_sub_left_distrib (invOneScaled a) 1 (C' a * X), mul_invOneScaled_scale_shifts]
+    simp [invOneScaled]
+
+end invOneScaled
+
+/-
+  `A.shift = 2 * A + (1 - X)⁻¹`
+-/
+theorem left_eq_right : A.shift = 2 * A + invUnitsSub 1 := by
+  ext n
+  cases' n with n
+  · simp
+  · have : 2 * mk α = C' 2 * mk α := rfl
+    simp [A, this, coeff_C_mul (n + 1) (mk α) 2]
+
+/-
+  `A = X * (1 - X)⁻¹ * (1 - 2 * X)⁻¹`
+-/
+theorem recurrence : A = X * (invUnitsSub 1) * invOneScaled 2 := by
+  have := shift.shift_inv left_eq_right
+  simp at this
+  rw [←mul_comm X (2 * A + invUnitsSub 1), LeftDistribClass.left_distrib X (2*A) (invUnitsSub 1), ←mul_assoc] at this
+  have : A - X * 2 * A = X * invUnitsSub 1 := by exact sub_eq_of_eq_add' this
+  simp [mul_comm (X * 2) A] at this
+  rw [←(MvPowerSeries.mul_one A), mul_assoc, ←mul_assoc 1 X 2, ←(mul_sub_left_distrib A 1 (1*X*2))] at this
+  simp at this
+  have : A * (1-X*2)*(invOneScaled 2) = X * invUnitsSub 1 * (invOneScaled 2) := congrFun (congrArg HMul.hMul this) (invOneScaled 2)
+  rw [CommMonoid.mul_comm X 2] at this
+  have inverse_works : (1-2*X : ℚ⟦X⟧)*(invOneScaled 2) = 1 := invOneScaled_inv 2
+  simp [mul_assoc, inverse_works] at this
+  simp [←mul_assoc] at this
+  trivial
