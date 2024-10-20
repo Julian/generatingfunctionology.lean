2afdc071739cffbfc917fe2cf105f27c5321c67b
Julian Berman <Julian@GrayVines.com>
2024-10-19 22:31:57 -0400

    Prove 1/3 of what's needed for the second recurrence.

--- Generatingfunctionology/Basic.lean
+++ Generatingfunctionology/Basic.lean
@@ -1,4 +1,4 @@
-import Mathlib.RingTheory.PowerSeries.Inverse
+import Mathlib.RingTheory.PowerSeries.Derivative
 import Mathlib.RingTheory.PowerSeries.WellKnown
 
 noncomputable section
@@ -26,12 +26,18 @@ abbrev constantCoeff' := constantCoeff α
   i.e. it is the series obtained by "dividing" all terms by X,
   which we now will denote `φ/ₓ` via Lean notation.
 -/
-abbrev shift : PowerSeries α := mk fun n ↦ coeff' (n + 1) φ
+abbrev shift : α⟦X⟧ := mk fun n ↦ coeff' (n + 1) φ
 
 notation φ " /ₓ " => shift φ
 
-/--
+/-- Unfold the definition of shift. -/
+lemma shift_def : φ/ₓ = (mk fun n ↦ coeff' (n + 1) φ : α⟦X⟧) := rfl
 
+/-- Shifting a power series is the same as the power series obtained by shifting the *series*. -/
+lemma mk_shift_eq_mk_add {f : ℕ → α}  : (mk f)/ₓ = mk (fun n ↦ f (n + 1)) := by
+  ext n; cases n <;> simp
+
+/--
   If `φ = a₀ + a₁ * X + a₂ * X^2 + ⋯` then
      `φ/ₓ * X + a₀ = φ`.
 -/
@@ -41,7 +47,8 @@ lemma shift_mul_X_add : φ/ₓ * X + (C' (constantCoeff' φ)) = φ := by
 variable {φ φ'}
 
 /-- Given `φ = a₀ + a₁ * X + a₂ * X^2 + ⋯`, if `φ' = φ/ₓ` then `φ' * X + a₀ = φ`. -/
-lemma shift_inv (h: φ/ₓ = φ') : φ = φ' * X + (C' (constantCoeff' φ)) := by rw [←h, shift_mul_X_add]
+lemma eq_mul_X_add_of_shift (h: φ/ₓ = φ') : φ = φ' * X + (C' (constantCoeff' φ)) := by
+  rw [←h, shift_mul_X_add]
 
 end PowerSeries
 
@@ -50,16 +57,24 @@ open PowerSeries
 -- GENERALIZEME: Can this be Semiring?
 variable [Ring α]
 
+lemma shift_inv : φ/ₓ * X = φ - (C' (constantCoeff' φ)) := by
+  ext n; cases n <;> simp
+
+lemma shift_sub_const (k : α) : (φ - C' k)/ₓ = φ/ₓ := by
+  ext n; cases n <;> simp
+
 section invOneScaled
 
 /--
   The power series for `(1 - a*X)⁻¹`.
   (It's equal to `1 + a * X + a^2 * X^2 + ...`)
 -/
-abbrev invOneScaled (a : α) : PowerSeries α := mk (a ^ ·)
+abbrev invOneScaled (a : α) : α⟦X⟧ := mk (a ^ ·)
 
 abbrev extractInvOneScaled (x : α⟦X⟧) : α⟦X⟧ := invOneScaled <| -(coeff' 1 x)
 notation a "/ ( " x " ) " => a * extractInvOneScaled x
+
+lemma extractInvOne : (1 / (1 - X) : α⟦X⟧) = invOneScaled 1 := by simp [extractInvOneScaled]
 
 section mkOneSpellings
 
@@ -107,6 +122,42 @@ theorem invOneScaled_inv_right (a : α) : 1 / (1 - C' a * X) * (1 - C' a * X) = 
   · simp
   · rw [mul_sub_left_distrib, mul_invOneScaled_scale_shifts]
     simp
+
+section derivative
+
+variable {R: Type*} [CommRing R]
+
+lemma derivative_mk (f : R⟦X⟧) : d⁄dX R f = mk fun n ↦ coeff R (n + 1) f * (n + 1) := rfl
+
+/--
+  `X + 2X^2 + 3X ^ 3 + ⋯ = X * d⁄dX (1 + X + X^2 + ⋯)`
+-/
+lemma X_mul_deriv_mk_one : (mk (·) : R⟦X⟧) = X * d⁄dX R (mk 1) := by
+  ext n; cases n <;> simp [derivative_mk]
+
+-- GENERALIZEME: nth deriv
+lemma invOneScaled_deriv : d⁄dX R (1 / (1 - X)) = (1 / (1 - X)) ^ 2 :=
+  have := calc d⁄dX R (1 / (1 - X))
+      = (mk (· + 1) : R⟦X⟧) := by simp [derivative_mk]
+    _ = (mk (·) : R⟦X⟧) + (mk 1) := by ext; simp
+    _ = (X : R⟦X⟧) * d⁄dX R (1 / (1 - X)) + (mk 1) := by ext n; cases n <;> simp [coeff_succ_X_mul, derivative_mk]
+
+  by
+    have : (d⁄dX R (1 / (1 - X))) - X * d⁄dX R (1 / (1 - X)) = 1 / (1 - X) := by
+      rw [extractInvOne, eq_mkOne] at this ⊢
+      exact (eq_sub_of_add_eq' this.symm).symm
+    have h : (d⁄dX R (1 / (1 - X))) * (1 - X) = (1 / (1 - X)) := by
+      have foo : (d⁄dX R) (1 * extractInvOneScaled (1 - X)) - X * (d⁄dX R) (1 * extractInvOneScaled (1 - X)) = 1 * (d⁄dX R) (1 * extractInvOneScaled (1 - X)) - X * (d⁄dX R) (1 * extractInvOneScaled (1 - X)) := by ring
+      rwa [foo, ←mul_sub_right_distrib, mul_comm] at this
+    have h' : d⁄dX R (1 / (1 - X)) * (1 - X) * 1 / (1 - X) = (1 / (1 - X)) * 1 / (1 - X) := congr($h * (1 / (1 - X)))
+    have h'' : (1 / (1 - X) : R⟦X⟧) * 1 / (1 - X) = (1 / (1 - X)) ^ 2 := by rw [extractInvOne, eq_mkOne, pow_two]
+    have h''' : (1 - X : R⟦X⟧) * (1 * extractInvOneScaled (1 - X)) = 1 := by
+      have bla := invOneScaled_inv (1 : R)
+      have bla' : C' 1 * X = (1 : R⟦X⟧) * X := rfl
+      rwa [bla', one_mul] at bla
+    rwa [h'', mul_assoc, h''', mul_one] at h'
+
+end derivative
 
 end invOneScaled
 
--- Generatingfunctionology/Chapter1.lean
+++ Generatingfunctionology/Chapter1.lean
@@ -2,6 +2,8 @@ import Generatingfunctionology.Basic
 
 open PowerSeries
 
+namespace «1.2»
+
 abbrev α : ℕ → ℚ
 | n + 1 => 2 * (α n) + 1
 | 0 => 0
@@ -19,7 +21,7 @@ theorem left_eq_right : A/ₓ = 2 • A + 1 / (1 - X) := by ext n; cases n <;> s
 -/
 theorem A_eq : A = X * 1 / (1 - X) * 1 / (1 - 2 • X) :=
   have := sub_eq_of_eq_add' <| calc
-    _ = (2 • A + 1 / (1 - X)) * X + C' (constantCoeff' A) := shift_inv left_eq_right
+    _ = (2 • A + 1 / (1 - X)) * X + C' (constantCoeff' A) := eq_mul_X_add_of_shift left_eq_right
     _ = (C' 2 * A + invOneScaled 1) * X := by simp [extractInvOneScaled, two_eq_C]
     _ = X * C' 2 * A + X * invOneScaled 1 := by ring
   by
@@ -42,3 +44,46 @@ theorem A.pfd_eq : (X : ℚ⟦X⟧) * (2 / (1 - 2 • X) - 1 / (1 - X)) = mk (2 
   ext n; cases n <;> simp [pow_succ, pow_mul_comm', two_eq_C]
 
 theorem coeff_alpha : α = (2 ^ · - 1) := ext_mk <| A.pfd_eq ▸ A.pfd ▸ A_eq
+
+end «1.2»
+
+namespace «1.3»
+
+abbrev α : ℕ → ℚ
+| n + 1 => 2 * (α n) + n
+| 0 => 1
+
+/-- A(x) -/
+abbrev A : ℚ⟦X⟧ := mk α
+
+/--
+  `(A - 1)/ₓ = 2 * A + X * d⁄dX (1 - X)⁻¹`
+-/
+theorem left_eq_right : (A - 1)/ₓ = 2 • A + X * (1 / (1 - X)) ^ 2 := by
+  calc (A - 1)/ₓ
+    _ = (A - C' (constantCoeff' A))/ₓ := rfl
+    _ = mk fun n ↦ 2 * (α n) + n := by rw [shift_sub_const, mk_shift_eq_mk_add]
+    _ = (mk fun n ↦ 2 * (α n)) + (mk (·) : ℚ⟦X⟧) := rfl
+    _ = 2 • A + (mk (·) : ℚ⟦X⟧) := by ext n; cases n <;> simp [two_eq_C]  -- GENERALIZEME...
+    _ = 2 • A + X * d⁄dX ℚ (mk 1) := by rw [X_mul_deriv_mk_one]
+    _ = 2 • A + X * (1 / (1 - X)) ^ 2 := by rw [←invOneScaled_deriv, ←eq_mkOne, extractInvOne]
+
+theorem A_eq : A = (1 - 2 • X + 2 • X ^ 2 : ℚ⟦X⟧) * (1 / (1 - X) ^ 2) * (1 / (1 - 2 • X)) := by
+  have : (A - C' (constantCoeff' A)) /ₓ = 2 • A + X * (1 * extractInvOneScaled (1 - X)) ^ 2 := left_eq_right
+  rw [sub_eq_iff_eq_add.mp <| eq_mul_X_add_of_shift this, extractInvOne, one_mul]
+  sorry
+
+theorem A.pfd :
+  (1 - 2 • X + 2 • X ^ 2 : ℚ⟦X⟧) * (1 / (1 - X) ^ 2) * (1 / (1 - 2 • X)) =
+  ((-1) / (1 - X) ^ 2 : ℚ⟦X⟧) + (2  / (1 - 2 • X) : ℚ⟦X⟧) :=
+  sorry
+
+/-- Find the coefficients of the partial fraction decomposition version of A -/
+theorem A.pfd_eq :
+  ((-1) / (1 - X) ^ 2 : ℚ⟦X⟧) + (2  / (1 - 2 • X) : ℚ⟦X⟧) =
+    mk (fun n ↦ 2 ^ (n + 1) - n - 1 : ℕ → ℚ) := by
+  ext n; cases n <;> sorry
+
+theorem coeff_alpha : α = (fun n ↦ 2 ^ (n + 1) - n - 1 : ℕ → ℚ) := ext_mk <| A.pfd_eq ▸ A.pfd ▸ A_eq
+
+end «1.3»
